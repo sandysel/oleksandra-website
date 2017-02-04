e24f9729054ef4da026de9d5ace7cff4e460e446
Oleksandra Seletska <seletska@gmail.com>
2017-02-04 08:07:54 +0000

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,20 @@
+
+
+/* $ global*/
+ var $;
+
+$(function() {
+
+    var headgreeting = $(".headgreeting");
+    var fadeIndex = -1;
+    
+    function showNext() {
+        ++fadeIndex;
+        headgreeting.eq(fadeIndex % headgreeting.length)
+            .show(2000)
+            .delay(2000)
+            .hide(2000, showNext);
+    }
+    
+    showNext();
+})();
