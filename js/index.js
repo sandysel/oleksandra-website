11829ae01d03787dc86b18fc3bd14f57594b62b0
Oleksandra Seletska <seletska@gmail.com>
2017-02-06 14:05:10 +0000

    Small changes

--- js/index.js
+++ js/index.js
@@ -6,40 +6,40 @@
 /* $ global*/
  var $;
 
-// (function() {
+(function() {
 
-//     var headgreeting = $(".headgreeting");
-//     var fadeIndex = -1;
+    var headgreeting = $(".headgreeting");
+    var fadeIndex = -1;
     
-//     function showNext() {
-//         ++fadeIndex;
-//         headgreeting.eq(fadeIndex % headgreeting.length)
-//             .show(3000)
-//             .delay(3000)
-//             .hide(3000, showNext);
-//     }
+    function showNext() {
+        ++fadeIndex;
+        headgreeting.eq(fadeIndex % headgreeting.length)
+            .show(3000)
+            .delay(3000)
+            .hide(3000, showNext);
+    }
 
-//     showNext();
-// })();
+    showNext();
+})();
 
 
 var x = document.getElementById("contactMe");
 
 
-         $(document.getElementById("contactme")).click(function() {
-                $("#contactme").toggle();
-            }); 
+        //  $(document.getElementById("contactme")).click(function() {
+        //         $("#contactme").toggle();
+        //     }); 
 
 
 
             
-            // $('#contactCheckbox').click(function() {
-            //     if ($(this.is(':checked'))) {
-            //         $("#contactme").show();
-            //     } else {
-            //         $("#contactme").hide();
-            //     }
-            // }); 
+            $('#contactCheckbox').click(function() {
+                if ($(this.is(':checked'))) {
+                    $("#contactme").show();
+                } else {
+                    $("#contactme").hide();
+                }
+            }); 
 
 
 // $('#contactCheckbox').click(function() {
